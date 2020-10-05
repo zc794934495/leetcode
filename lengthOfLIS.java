815ab648e199d43f2973649c92be2d00261aa683
Zc794934495 <794934495@qq.com>
2020-10-05 20:56:27 +0800

    给定一个无序的整数数组，找到其中最长上升子序列的长度。

---
+++ lengthOfLIS.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int len = nums.length;
+        if(len <= 1){
+            return len;
+        }
+        int[] tail = new int[len];
+        tail[0] = nums[0];
+        int end = 0;
+        for(int i = 1;i < len;i++){
+            if(nums[i] > tail[end]){
+                end++;
+                tail[end] = nums[i];
+            }else{
+                int left = 0;
+                int right = end;
+                while(left < right){
+                    int mid = left + ((left + right) >>> 1);
+                    if(tail[mid] < nums[i]){
+                        left = mid + 1;
+                    }else{
+                        right = mid;
+                    }
+                }
+                tail[left] = nums[i];
+            }
+        }
+        end++;
+        return end;
+    }
+}
