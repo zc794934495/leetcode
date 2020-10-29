8033ec4f1341c6cb63d57a5e687bbc86fc55578a
794934495@qq.com <794934495@qq.com>
2020-10-29 18:27:37 +0800

    给定一个二叉树，它的每个结点都存放一个 0-9 的数字，每条从根到叶子节点的路径都代表一个数字。例如，从根到叶子节点路径 1->2->3 代表数字 123。计算从根到叶子节点生成的所有数字之和。 说明: 叶子节点是指没有子节点的节点。 来源：力扣（LeetCode）

---
+++ sumNumbers.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        return dfs(root,0);
+    }
+    public int dfs(TreeNode root,int sum) {
+        if(root == null) {
+            return 0;
+        }
+        sum = sum * 10 + root.val;
+        if(root.left == null && root.right == null) {
+            return sum;
+        }else {
+            return dfs(root.left,sum) + dfs(root.right,sum);
+        }
+    }
+}
